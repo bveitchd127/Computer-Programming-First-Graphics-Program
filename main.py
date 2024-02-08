19f924e9c6d8551a74ea1baaa661519729774aaa
bveitchd127 <bveitch@d127.org>
2024-02-08 11:45:50 -0600

    Changed movement from events to key dictionary

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ running = True
 playerx = 640
 playery = 360
 
+# 300 px / s
+playerSpeed = 5
+
 while running:
     dt = clock.tick(60) # waits for 17ms
 
@@ -16,16 +19,35 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                playery = playery - 20
-            if event.key == pygame.K_DOWN:
-                playery = playery + 20
+        # elif event.type == pygame.KEYDOWN:
+        #     # if event.key == pygame.K_UP:
+        #     #     playery = playery - 20
+        #     # if event.key == pygame.K_DOWN:
+        #     #     playery = playery + 20
+            
+        #     if event.key == pygame.K_LEFT:
+        #         playerx = playerx - 20
+        #     if event.key == pygame.K_RIGHT:
+        #         playerx = playerx + 20
 
+    # Computation
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP]:
+        playery = playery - playerSpeed
+    
+    if keys[pygame.K_DOWN]:
+        playery = playery + playerSpeed
+    
+    if keys[pygame.K_LEFT]:
+        playerx = playerx - playerSpeed
+    
+    if keys[pygame.K_RIGHT]:
+        playerx = playerx + playerSpeed
+                
+    
+    # Render
     screen.fill("purple")
-
     pygame.draw.circle(screen, "red", (playerx, playery), 50)
-
     pygame.display.update()
 
 pygame.quit()
